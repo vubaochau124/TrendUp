2258355ee3d143461e9311936acfb3721cf54344
truongquynhgiang03 <21021485@vnu.edu.vn>
2024-11-21 19:17:30 +0700

    add files backend + edit ui add-product
    - backend: 5:10:42 6:22:22
    trong đó chưa setup database: 1 file .js trong config, 2 file .js trong model
    - chỉnh sửa ui của add-product, còn backend của add-product: từ 8:54:44 đến 9:02:48 (chưa làm đc vì thiếu nhiều thứ như: backendUrl ở App.jsx, …)

--- back-end/server.js
+++ back-end/server.js
@@ -1,16 +1,21 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
 
 //app config
 const app = express()
 const port = process.env.PORT || 4000
+connectCloudinary()
 
 //middleware
 app.use(express.json())
 app.use(cors())
 
 //api endpoints
+app.use('/api/user',userRouter)
+
 app.get('/', (req, res) => {
     res.send('Hello World')
 });
