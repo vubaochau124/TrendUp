4fb1a52a9cf02acda4271099a08c902a043add73
vubaochau124 <baochauvu124@gmail.com>
2024-12-06 15:29:07 +0700

    cart

--- front-end/src/context/ShopContext.jsx
+++ front-end/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { products } from '../assets/assets';
+import { toast } from 'react-toastify';
 
 export const ShopContext = React.createContext();
 
@@ -9,10 +10,50 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10;
     const [search, setSearch] = React.useState('');
     const [showSearch, setShowSearch] = React.useState(true);
+    const [cartItems, setCartItems] = React.useState({});
+
+    const addToCart = async (itemId,size) => {
+        if (!size) {
+            toast.error('Please select a size');
+            return;
+        }
+        let cartData = structuredClone(cartItems);
+
+        if (cartData[itemId]) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            } else {
+                cartData[itemId][size] = 1;
+            }
+        }
+        else {
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+        setCartItems(cartData);
+    }
+
+    const getCartCount = () => {
+        let totalCoount = 0;
+        for (const items in cartItems) {
+            for (const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalCoount += cartItems[items][item];
+                    }
+                } catch (error) {
+                    
+                }
+            }
+        }
+        return totalCoount;
+    }
 
     const value = {
         products, currency, delivery_fee,
-        search, setSearch, showSearch, setShowSearch
+        search, setSearch, showSearch, setShowSearch,
+        cartItems, addToCart,
+        getCartCount
     }
 
     return (
--- front-end/src/pages/Cart.jsx
+++ front-end/src/pages/Cart.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useContext, useEffect, useState} from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const Cart = () => {
+    const {product, currency, cartItems } = useContext(ShopContext);
+    const [cartData, setCartData] = useState([]);
+
+    useEffect(() => {
+
+      const tempData = [];
+      for (const items in cartItems) {
+        for (const item in cartItems[items]) {
+          if (cartItems[items][item] > 0) {
+            tempData.push({
+              _id: items,
+              size: item,
+              quantity: cartItems[items][item]
+            })
+          }
+        }
+      }
+    },[cartItems])
   return (
-    <div>Cart</div>
+    <div>
+
+    </div>
   )
 }
 
