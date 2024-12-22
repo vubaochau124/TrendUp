b7c18c9265f2b251545a4c782c97a52d698b229b
vubaochau124 <baochauvu124@gmail.com>
2024-12-23 02:34:30 +0700

    Feat: Inventory listing

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -48,20 +48,27 @@ const App = () => {
             <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
               <Routes>
                 <Route path='/' element={<Employee token={token}/>} />
+                
                 <Route path='/Employee_manage/List' element={<Employee token={token}/>} />
                 <Route path='/Employee_manage/Add' element={<Employee_add token={token}/>} />
                 <Route path='/Employee_manage/Edit/:employee_id' element={<Employee_edit token={token}/>} />
+                
                 <Route path='/Customer' element={<Customer token={token}/>} />
+
                 <Route path='/Order_manage' element={<Order token={token}/>} />
                 <Route path='/Order_detail/:id' element={<Order_detail token={token}/>} />
+
                 <Route path='/Product_manage/List' element={<Product token={token}/>} />
                 <Route path='/Product_manage/Add' element={<Product_add token={token}/>} />
                 <Route path='/Product_manage/Edit/:product_id' element={<Product_edit token={token}/>} />
+
                 <Route path='/Category_manage/List' element={<Category token={token}/>} />
                 <Route path='/Category_manage/Add' element={<Category_add token={token}/>} />
                 <Route path='/Category_manage/Edit/:id' element={<Category_edit token={token}/>} />
                 <Route path='/Category_manage/ListProduct/:id' element={<Category_product token={token}/>} />
+
                 <Route path='/Inventory' element={<Inventory token={token}/>} />
+
                 <Route path='/Import_manage' element={<Import_manage token={token}/>} />
               </Routes>
             </div>
