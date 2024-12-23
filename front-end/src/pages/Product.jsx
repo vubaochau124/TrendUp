e3e7a2fac222137cb9deaa3f95e12e79521cb7a3
vubaochau124 <baochauvu124@gmail.com>
2024-12-23 10:41:38 +0700

    Disp: Change website and product display

--- front-end/src/pages/Product.jsx
+++ front-end/src/pages/Product.jsx
@@ -45,14 +45,14 @@ const Product = () => {
         {/* Product Info */}
         <div className='flex-1'>
           <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
-          <div className='flex items-center gap-1 mt-2'>
+          {/* <div className='flex items-center gap-1 mt-2'>
               <img src={assets.star_icon} alt="" className='w-3 5'/>
               <img src={assets.star_icon} alt="" className='w-3 5'/>
               <img src={assets.star_icon} alt="" className='w-3 5'/>
               <img src={assets.star_icon} alt="" className='w-3 5'/>
               <img src={assets.star_dull_icon} alt="" className='w-3 5'/>
               <p className='p1-2'>(122)</p>
-          </div>
+          </div> */}
           <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
           <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
           <div className='flex flex-col gap-4 my-8'>
@@ -82,18 +82,20 @@ const Product = () => {
       </div>
 
       {/* Description & Review */}
-      <div className='mt-20'>
+      {/* <div className='mt-20'>
         <div className='flex'>
           <b className='border px-5 py-3 text-sm'>Description</b>
           <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
         </div>
         <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'> 
-          <p>something</p>
-          <p>something</p>
+          {productData.description}
         </div>
+      </div> */}
+      <div>
+        <hr />
       </div>
-
       {/* related product */}
+      
       <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
     </div>
   ) : <div className='opacity-0'></div>
